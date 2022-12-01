4b33236fadb299e7be612e605dc65f274117941f
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2022-12-01 18:29:51 -0300

    lexer exceptions

---
+++ br/com/gabrielluizsf/compiler/lexer/exceptions/EzLexicalException.java
@@ -0,0 +1,11 @@
+package br.com.gabrielluizsf.compiler.lexer.exceptions;
+
+public class EzLexicalException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+	public EzLexicalException(String msg){
+        super(msg);
+    }
+
+}
